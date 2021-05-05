0f25387f6d49926d1c4831c3ad32bf9fe42ef141
japsasir <japsasir@gmail.com>
2021-05-05 11:34:00 +0200

    Update ACME.sh

--- ACME.sh
+++ ACME.sh
@@ -12,4 +12,7 @@ sudo snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
 
 # Obtenemos el certificado y configuramos el servidor web Apache.
-sudo certbot --apache
+sudo certbot --apache -m demo@demo.es --agree-tos --no-eff-email -d jpadilla.sytes.net
+
+# Con el siguiente comando podemos comprobar que hay un temporizador en el sistema encargado de realizar la renovación de los certificados de manera automática.
+# systemctl list-timers
